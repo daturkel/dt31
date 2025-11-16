3775180b180e1cf2984a0b6df0f9904024a05738
Dan Turkel <daturkel@gmail.com>
2025-11-16 18:18:11 -0500

    Export Comment from dt31 top level

--- src/dt31/__init__.py
+++ src/dt31/__init__.py
@@ -7,3 +7,4 @@
 import dt31.instructions as I  # noqa: F401
 from dt31.cpu import DT31  # noqa: F401
 from dt31.operands import LC, L, Label, M, R  # noqa: F401
+from dt31.parser import Comment  # noqa: F401
